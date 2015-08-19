d6f61b438b7ed937ab3fba4e9e2ca7365df619ac
Bruce Stringer <bruce.stringer@rackspace.com>
2015-08-19 17:26:49 -0500

    Update run.sh to use gunicorn and nginx

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
-#!/bin/sh
-python ./manage.py syncdb
-python ./manage.py migrate
+#!/bin/bash
+python manage.py makemigrations
+python manage.py syncdb
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('adminuser', 'haven.wang@rackspace.com', 'j3lskj6kja8sd8jh5')" | python manage.py shell
-python ./manage.py runserver 0.0.0.0:8000
+./scripts/gunicorn_start.sh &
+nginx -g "daemon off;"
