4586fade0846dc6c6ca695d7f53a25ffe816df95
havenwang <Haven.Wang@rackspace.com>
2015-08-20 13:53:59 -0500

    clean up mysql settings file

--- readthedocs/settings/mysql.py
+++ readthedocs/settings/mysql.py
@@ -45,7 +45,6 @@ CELERY_RESULT_BACKEND = 'redis://cache:6379/0'
 SESSION_COOKIE_DOMAIN = None
 SESSION_COOKIE_HTTPONLY = False
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
-#CACHE_BACKEND = 'redis://'
 
 SLUMBER_USERNAME = 'adminuser'
 SLUMBER_PASSWORD = 'j3lskj6kja8sd8jh5'  # noqa: ignore dodgy check
@@ -63,18 +62,8 @@ HAYSTACK_CONNECTIONS = {
 DONT_HIT_DB = False
 NGINX_X_ACCEL_REDIRECT = True
 
-# If you are deployed behind a load-balancer or reverse-proxy server, and the
-# ``Strict-Transport-Security`` header is not being added to your responses,
-# it may be because Django doesn't realize that it's on a secure connection;
-# you may need to set the :setting:`SECURE_PROXY_SSL_HEADER` setting.
-#SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-
-#CELERY_ALWAYS_EAGER = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 FILE_SYNCER = 'readthedocs.privacy.backends.syncers.LocalSyncer'
-
-# allauth settings
-#ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
 
 MEDIA_ROOT = os.path.join(os.getcwd(), 'media')
 MEDIA_URL = '/media/'
@@ -90,3 +79,17 @@ if not os.environ.get('DJANGO_SETTINGS_SKIP_LOCAL', False):
         from local_settings import *  # noqa
     except ImportError:
         pass
+
+
+#CACHE_BACKEND = 'redis://'
+
+# allauth settings
+#ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
+
+#CELERY_ALWAYS_EAGER = True
+
+# If you are deployed behind a load-balancer or reverse-proxy server, and the
+# ``Strict-Transport-Security`` header is not being added to your responses,
+# it may be because Django doesn't realize that it's on a secure connection;
+# you may need to set the :setting:`SECURE_PROXY_SSL_HEADER` setting.
+#SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
