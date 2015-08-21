bd9eb1c35ede401de0e3ee634d8b24e5d7c04638
havenwang <Haven.Wang@rackspace.com>
2015-08-21 12:53:22 -0500

    take no input for collectstatic

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 python manage.py makemigrations
 python manage.py syncdb
 echo "from django.contrib.auth.models import User; User.objects.create_superuser('adminuser', 'haven.wang@rackspace.com', 'j3lskj6kja8sd8jh5')" | python manage.py shell
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 ./scripts/gunicorn_start.sh &
 nginx -g "daemon off;"
